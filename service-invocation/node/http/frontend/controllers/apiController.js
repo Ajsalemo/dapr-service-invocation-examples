266f2c12fcaecb2d066e491ba68f3809d9184418
Anthony Salemo <ajssalemo@gmail.com>
2022-08-31 14:45:47 -0400

    changing incorrect dapr url

--- service-invocation/node/http/frontend/controllers/apiController.js
+++ service-invocation/node/http/frontend/controllers/apiController.js
@@ -5,7 +5,7 @@ const router = Router();
 
 export const apiController = router.get("/", async (_req, res) => {
   try {
-    const { data } = await axios.get("http://localhost:3500/v1/invoke/backend/method/api/cars/get");
+    const { data } = await axios.get("http://localhost:3500/v1.0/invoke/backend/method/api/cars/get");
     res.json({ msg: data });
   } catch (error) {
     console.log(error);
