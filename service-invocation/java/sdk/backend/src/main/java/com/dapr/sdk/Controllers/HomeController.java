84d991ca00b69398c05c03c6b47646572ed72e1d
Anthony Salemo <ajssalemo@gmail.com>
2022-09-04 22:43:38 -0400

    adding controllers for the java/sdk application(s)

---
+++ service-invocation/java/sdk/backend/src/main/java/com/dapr/sdk/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.dapr.sdk.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    String message = "service-invocation-java-sdk-backend";
+
+    @GetMapping("/")
+    public String index() {
+        return message;
+    }
+}
---
+++ service-invocation/java/sdk/frontend/src/main/java/com/dapr/sdk/SdkApplication.java
@@ -0,0 +1,13 @@
+package com.dapr.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdkApplication.class, args);
+	}
+
+}
