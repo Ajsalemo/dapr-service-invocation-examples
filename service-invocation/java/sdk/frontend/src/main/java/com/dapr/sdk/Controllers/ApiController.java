07c2b7e2e0c7e276f474130cc52702f99854b67f
Anthony Salemo <ajssalemo@gmail.com>
2022-09-06 19:37:06 -0400

    trying to implement the proper return type for the frontend dapr call

--- service-invocation/java/sdk/frontend/src/main/java/com/dapr/sdk/Controllers/ApiController.java
+++ service-invocation/java/sdk/frontend/src/main/java/com/dapr/sdk/Controllers/ApiController.java
@@ -1,22 +1,27 @@
 package com.dapr.sdk.Controllers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.HttpExtension;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ApiController {
     String DAPR_BACKEND_SERVICE = "backend";
-    String DAPR_BACKEND_METHOD = "api/cars/get";
+    String DAPR_BACKEND_METHOD = "api/get/cars";
 
     @GetMapping("/api/dapr")
-    public Void apiController() throws Exception {
+    public Mono<String> apiController() throws Exception {
         try (DaprClient client = (new DaprClientBuilder()).build()) {
-            Void apiResponse = client
-                    .invokeMethod(DAPR_BACKEND_SERVICE, DAPR_BACKEND_METHOD, byte[].class, HttpExtension.GET).block();
+            Mono<String> apiResponse = client
+                    .invokeMethod(DAPR_BACKEND_SERVICE, DAPR_BACKEND_METHOD, null, HttpExtension.GET, String.class);
 
             System.out.println(apiResponse);
             return apiResponse;
